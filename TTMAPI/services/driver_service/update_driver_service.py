e5fb2ffa0592e103f4d4c5ef6b9d2c2b4932f591
Gustavo Vicuña G <gpvicuna@miuandes.cl>
2023-04-26 13:51:01 -0400

    Fix update driver

--- TTMAPI/services/driver_service/update_driver_service.py
+++ TTMAPI/services/driver_service/update_driver_service.py
@@ -5,11 +5,18 @@ from fastapi import HTTPException
 
 def update_driver_service(driver: Driver, dbid: str, logger):
     collection = getDB().drivers
+
     driver_cursor = collection.find_one({"dbid": dbid})
     if driver_cursor is not None:
+
+        if "" in driver.phrases:
+            driver.phrases.remove("")
+
         collection.replace_one(
             {"dbid": dbid},
             driver)
+
         return collection.find_one({"dbid": dbid})
     else:
+        logger.error("Driver not Found")
         raise HTTPException(status_code=404, detail="Driver not found")
