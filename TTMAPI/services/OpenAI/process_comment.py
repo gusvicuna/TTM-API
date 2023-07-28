a51661e0cf99e472f83aee7be75f1e03e378a685
Gustavo Vicuña G <gpvicuna@miuandes.cl>
2023-07-28 13:15:54 -0400

    fix(gpt_process): fix path to file

--- TTMAPI/services/OpenAI/process_comment.py
+++ TTMAPI/services/OpenAI/process_comment.py
@@ -9,67 +9,63 @@ openai.api_key = config["OPENAI_API_KEY"]
 
 
 def gpt_process(traintext: str, logger):
-    logger.debug("GPT Process")
+    full_result = {
+        "REQUERIMIENTO": 0,
+        "TIEMPOS": 0,
+        "ECONOMICOS": 0,
+        "APRENDIZAJE": 0,
+        "RAPIDEZ": 0,
+        "CONOCIMIENTO": 0,
+        "ORIENTACION": 0,
+        "EFECTIVIDAD COMUNICACIONAL": 0,
+        "PRESENCIA": 0,
+        "PROACTIVIDAD": 0,
+        "TRATO": 0,
+        "ACCIONES DE AMOR": 0,
+        "PRECIOS": 0,
+        "TASAS": 0,
+        "CONDICIONES": 0,
+        "CREDITO": 0,
+        "CONFIANZA": 0,
+        "HONESTIDAD": 0,
+        "RESPETO": 0,
+        "ECOLOGIA": 0,
+        "FACILIDAD": 0,
+        "FLUIDEZ": 0,
+        "PRACTICIDAD": 0,
+        "CALIDAD": 0,
+        "VARIEDAD": 0,
+        "DISENADOS PARA MI": 0,
+        "DISPONIBILIDAD": 0,
+        "TECNOLOGIA": 0,
+        "FACILES DE VENDER": 0,
+        "ENVOLTURA": 0,
+        "SABOR": 0,
+        "BAÑOS": 0,
+        "ESTACIONAMIENTOS": 0,
+        "MAQUINAS": 0,
+        "SUCURSALES": 0,
+        "LAYOUT": 0,
+        "SECTORES DE ATENCION": 0,
+        "APP": 0,
+        "WEB ": 0,
+        "MARCA": 0,
+        "COLABORADOR": 0,
+        "RELACIÓN COMERCIAL": 0,
+        "PRODUCTO_SERVICIO": 0,
+        "COMUNICACION_MKT": 0,
+        "DISTRIBUCIÓN": 0,
+        "INFRASTUCTURA_VIRTUAL": 0,
+        "INFRASTUCTURA_FISICA": 0,
+        "CONTACT_CENTER": 0,
+    }
+
+    prompt_instruction = "TTMAPI/services/OpenAI/prompt_instruction_simple.txt"
+
     try:
-        full_result = {
-            "REQUERIMIENTO": 0,
-            "TIEMPOS": 0,
-            "ECONOMICOS": 0,
-            "APRENDIZAJE": 0,
-            "RAPIDEZ": 0,
-            "CONOCIMIENTO": 0,
-            "ORIENTACION": 0,
-            "EFECTIVIDAD COMUNICACIONAL": 0,
-            "PRESENCIA": 0,
-            "PROACTIVIDAD": 0,
-            "TRATO": 0,
-            "ACCIONES DE AMOR": 0,
-            "PRECIOS": 0,
-            "TASAS": 0,
-            "CONDICIONES": 0,
-            "CREDITO": 0,
-            "CONFIANZA": 0,
-            "HONESTIDAD": 0,
-            "RESPETO": 0,
-            "ECOLOGIA": 0,
-            "FACILIDAD": 0,
-            "FLUIDEZ": 0,
-            "PRACTICIDAD": 0,
-            "CALIDAD": 0,
-            "VARIEDAD": 0,
-            "DISENADOS PARA MI": 0,
-            "DISPONIBILIDAD": 0,
-            "TECNOLOGIA": 0,
-            "FACILES DE VENDER": 0,
-            "ENVOLTURA": 0,
-            "SABOR": 0,
-            "BAÑOS": 0,
-            "ESTACIONAMIENTOS": 0,
-            "MAQUINAS": 0,
-            "SUCURSALES": 0,
-            "LAYOUT": 0,
-            "SECTORES DE ATENCION": 0,
-            "APP": 0,
-            "WEB ": 0,
-            "MARCA": 0,
-            "COLABORADOR": 0,
-            "RELACIÓN COMERCIAL": 0,
-            "PRODUCTO_SERVICIO": 0,
-            "COMUNICACION_MKT": 0,
-            "DISTRIBUCIÓN": 0,
-            "INFRASTUCTURA_VIRTUAL": 0,
-            "INFRASTUCTURA_FISICA": 0,
-            "CONTACT_CENTER": 0,
-        }
-
-        prompt_instruction = "TTMAPI/services/OpenAI/prompt_instruction_simple.txt"
-
-        logger.debug("Prompt instruction")
 
         with open(prompt_instruction, 'r') as file:
             prompt_instruction: str = file.read()
-
-        logger.debug("OPENAI")
 
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
@@ -82,12 +78,9 @@ def gpt_process(traintext: str, logger):
             frequency_penalty=0,
             presence_penalty=0
         )
-
-        logger.debug("RESULT")
-
         result = response['choices'][0]['message']['content']
 
-        logger.debug(f"gptresponse: {result}")
+        logger.info(f"gptresponse: {result}")
 
         json_result = json.loads(result)
         for key in json_result:
