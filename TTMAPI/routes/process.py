2abffa51fbe31288207438f807471a0321be626d
Gustavo Vicuña G <gpvicuna@miuandes.cl>
2023-09-04 14:27:22 -0300

    update

--- TTMAPI/routes/process.py
+++ TTMAPI/routes/process.py
@@ -3,10 +3,12 @@ from fastapi import APIRouter
 from TTMAPI.helpers.log import get_logger
 from TTMAPI.models.driver import Driver
 from TTMAPI.schemas.driver import matchedDriversSchema
-from TTMAPI.services import driver_service
+from TTMAPI.services import MongoDB
 from TTMAPI.services.OpenAI.fix_grammar import fix_grammar
 from TTMAPI.services.OpenAI.API.gpt_simple_process import\
     gpt_simple_process
+from TTMAPI.services.OpenAI.API.generate_description import\
+    generate_description
 
 router = APIRouter()
 logger = get_logger(__name__)
@@ -24,13 +26,14 @@ async def post_playground_process(
         ):
 
     logger.info(
-        f"POST {base_route}/playground_process  traintext='{trainText}' ttm_process={ttm_process} gpt_process={gpt_process}")
+        f"POST {base_route}/playground_process traintext='{trainText}' " +
+        " ttm_process={ttm_process} gpt_process={gpt_process}")
 
     if (fixGrammar):
         trainText = fix_grammar(traintext=trainText)
         logger.debug(f"fixed traintext ='{trainText}'")
 
-    drivers_cursor = driver_service.get_all_drivers(
+    drivers_cursor = MongoDB.get_all_drivers(
         logger=logger)
     drivers = []
     for driver_cursor in drivers_cursor:
@@ -69,7 +72,7 @@ async def get_TTM_simple_match(
         trainText = fix_grammar(traintext=trainText)
         logger.info(f"fixed traintext ='{trainText}'")
 
-    drivers_cursor = driver_service.get_all_drivers(
+    drivers_cursor = MongoDB.get_all_drivers(
         logger=logger)
     components = {}
     for driver_cursor in drivers_cursor:
@@ -95,8 +98,7 @@ async def get_GPT_simple_match(
         trainText = fix_grammar(traintext=trainText)
         logger.debug(f"fixed traintext ='{trainText}'")
 
-    drivers_cursor = driver_service.get_all_drivers(
-        logger=logger)
+    drivers_cursor = MongoDB.get_all_drivers(logger=logger)
     drivers = []
     for driver_cursor in drivers_cursor:
         driver = Driver(**driver_cursor)
@@ -104,3 +106,34 @@ async def get_GPT_simple_match(
 
     result = gpt_simple_process(trainText, drivers=drivers, logger=logger)
     return result
+
+
+@router.post("/create_description")
+async def create_description(
+        component_name: str
+        ):
+
+    logger.info(f"POST {base_route}/create_description  component_name='{component_name}'")
+
+    drivers_cursor = MongoDB.get_all_drivers(logger=logger)
+    drivers = []
+    for driver_cursor in drivers_cursor:
+        driver = Driver(**driver_cursor)
+        drivers.append(driver)
+
+    for driver in drivers:
+        for component in driver.components:
+            if component.name == component_name:
+                selected_component = component
+
+    selected_component.description = generate_description(
+        name=selected_component.name,
+        phrases=selected_component.phrases,
+        logger=logger)
+    for driver in drivers:
+        for component in driver.components:
+            if component.name == component_name:
+                MongoDB.update_driver(
+                    driver=driver.dict(),
+                    dbid=driver.dbid,
+                    logger=logger)
