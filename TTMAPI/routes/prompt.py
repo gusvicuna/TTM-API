eb3dc8ad4fb0670c3a5f1901de7cc86290204eeb
Gustavo Vicuña G <gpvicuna@miuandes.cl>
2023-10-19 16:39:19 -0300

    add logger info to prompt routes

--- TTMAPI/routes/prompt.py
+++ TTMAPI/routes/prompt.py
@@ -18,6 +18,7 @@ async def get_prompt_instruction(prompt_id: int):
     """
     Get prompt from get_prompt_service.py and return it
     """
+    logger.info(f"GET {base_route}/{prompt_id}")
     prompt_cursor = get_prompt_service(prompt_id=prompt_id, logger=logger)
     prompt = Prompt(**prompt_cursor)
     return prompt
@@ -28,6 +29,7 @@ async def update_prompt_instruction(prompt_id: int, prompt: Prompt):
     """
     Update prompt from update_prompt_service.py and return it
     """
+    logger.info(f"PUT {base_route}/{prompt_id} prompt='{prompt}'")
     prompt_cursor = update_prompt_service(
         prompt_id=prompt_id,
         prompt=prompt,
