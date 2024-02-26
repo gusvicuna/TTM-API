447cbb6a5e0d699e77897575261135afcadd624e
Gustavo Vicuña G <gpvicuna@miuandes.cl>
2024-02-26 16:24:15 -0300

    feat(get_processed_answers): add ttm_priority conjunto

--- TTMAPI/services/PlataformaAPM/get_processed_answers_service.py
+++ TTMAPI/services/PlataformaAPM/get_processed_answers_service.py
@@ -41,14 +41,19 @@ def get_processed_answer(token, session, logger):
         driver_result = {}
         driver_result["driver_id"] = driver.id
         driver_result["components"] = []
-        # Creamos los conjuntos de resultados de componentes para TTM y GPT
+        # Creamos los conjuntos de resultados de componentes para TTM y GPT,
+        # y uno para guardar los componentes con prioridad en TTM
         gpt_component_result = []
         ttm_component_result = []
+        priority_ttm_component_result = []
         for component in driver.components:
             component_result_gpt = {}
             component_result_ttm = {}
+            component_result_priority_ttm = {}
             component_result_gpt["component_id"] = component.id
             component_result_ttm["component_id"] = component.id
+            component_result_priority_ttm["component_id"] = component.id
+
             # Obtenemos la información de la respuesta del componente
             answer_component = get_answer_component_service(
                 session, token, component, driver, survey, logger)
@@ -84,17 +89,19 @@ def get_processed_answer(token, session, logger):
                 else:
                     resultado = 0
 
-            if component_result_ttm["resultado"] == 0 and\
-                    component_result_gpt["resultado"] == 0:
+            if resultado == 0:
                 continue
 
             if answer_component.gpt_process != 0:
                 component_result_gpt["resultado"] = resultado
             if answer_component.ttm_process != 0:
                 component_result_ttm["resultado"] = resultado
+            if component.ttm_priority:
+                component_result_priority_ttm["resultado"] = resultado
 
             component_result_ttm["uts"] = []
             component_result_gpt["uts"] = []
+            component_result_priority_ttm["uts"] = []
 
             # Se hace el mismo proceso para cada UT
             for ut in uts:
@@ -138,6 +145,7 @@ def get_processed_answer(token, session, logger):
                 if ut_result["components"]:
                     component_result_ttm["uts"].append(ut_result)
                     component_result_gpt["uts"].append(ut_result)
+                    component_result_priority_ttm["uts"].append(ut_result)
 
             # Si existe un componente ut default en el driver, se guarda en el
             # resultado
@@ -152,6 +160,7 @@ def get_processed_answer(token, session, logger):
                 default_ut_driver["components"].append(default_ut_component)
                 component_result_ttm["uts"].append(default_ut_driver)
                 component_result_gpt["uts"].append(default_ut_driver)
+                component_result_priority_ttm["uts"].append(default_ut_driver)
 
             # Si no hay componentes de UT, se agrega el default
             # (chequeamos el conjunto de TTM ya que es el mismo para ambos)
@@ -166,22 +175,31 @@ def get_processed_answer(token, session, logger):
                 default_ut_driver["components"].append(default_ut_component)
                 component_result_ttm["uts"].append(default_ut_driver)
                 component_result_gpt["uts"].append(default_ut_driver)
+                component_result_priority_ttm["uts"].append(default_ut_driver)
 
             # Se agregan los resultados a los conjuntos de resultados
             if answer_component.gpt_process != 0:
                 gpt_component_result.append(component_result_gpt)
             if answer_component.ttm_process != 0:
                 ttm_component_result.append(component_result_ttm)
+            if component.ttm_priority and answer_component.ttm_process != 0:
+                priority_ttm_component_result.append(
+                    component_result_priority_ttm)
 
         # Si tuvo marca de TTM y menos de 6 palabras,
         # se agrega el conjunto TTM
         if (len(answer.answer_text.split(" ")) < 6 and ttm_component_result):
             driver_result["components"] = ttm_component_result
             results["codificacion"].append(driver_result)
-        # Si tuvo marca de TTM y GPT,
+        # Sino, si tuvo marca de TTM y GPT,
         # se agrega el conjunto TTM
         elif (ttm_component_result and gpt_component_result):
             driver_result["components"] = ttm_component_result
+            results["codificacion"].append(driver_result)
+        # Sino, si el conjunto de ttm con prioridad no está vacío,
+        # se agrega el conjunto con prioridad
+        elif priority_ttm_component_result:
+            driver_result["components"] = priority_ttm_component_result
             results["codificacion"].append(driver_result)
         # En caso contrario, no se guarda en la codificación
 
